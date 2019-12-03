d4086846b29618541c123906585cd461b07044cf
trongquang <aovanquang@gmail.com>
2019-12-03 14:33:01 +0700

    create layout

---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,83 @@
+<nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="">Quản lý</a>
+            </div>
+            <ul class="nav navbar-top-links navbar-right">
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                        <i class="fa fa-user fa-fw"></i>  <i class="fa fa-caret-down"></i>
+                    </a>
+                    <ul class="dropdown-menu dropdown-user">
+                        <li>
+                            <a href=""><i class="fa fa-user fa-fw"></i>
+                            </a>
+                        </li>
+                        <li class="divider"></li>
+                        <li><a href=""><i class="fa fa-sign-out fa-fw"></i> Logout</a>
+                        </li>
+                        @endif
+                    </ul>
+                </li>
+            </ul>
+            <div class="navbar-default sidebar" role="navigation">
+                <div class="sidebar-nav navbar-collapse">
+                    <ul class="nav" id="side-menu">
+                        <div class="space20">&nbsp;</div>
+                        <br>
+                        <li>
+                            <a href=""><i class="fa fa-dashboard fa-fw"></i> Trang tổng quan</a>
+                        </li>
+                        <li>
+                            <a href="javascript:void(0)"><i class="fa fa-cube fa-fw"></i> Danh mục<span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+                                <li>
+                                    <a href="">Danh sách</a>
+                                </li>
+                                <li>
+                                    <a href="">Thêm</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="javascript:void(0)"><i class="fa fa-cube fa-fw"></i> Sản phẩm<span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+                                <li>
+                                    <a href="">Danh sách</a>
+                                </li>
+                                <li>
+                                    <a href="">Thêm</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="javascript:void(0)"><i class="fa fa-bar-chart-o fa-fw"></i> Nhập hàng<span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+                                <li>
+                                    <a href="">Danh sách</a>
+                                </li>
+                                <li>
+                                    <a href="">Thêm</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="javascript:void(0)"><i class="fa fa-users fa-fw"></i> Tài khoản<span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+                                <li>
+                                    <a href="">Danh sách</a>
+                                </li>
+                                <li>
+                                    <a href="">Thêm</a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Trang admin</title>
+    <base href="{{asset('')}}" >
+    <link href="admin_asset/bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="admin_asset/bower_components/metisMenu/dist/metisMenu.min.css" rel="stylesheet">
+    <link href="admin_asset/dist/css/sb-admin-2.css" rel="stylesheet">
+    <link href="admin_asset/bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="http://cdn.datatables.net/1.10.4/css/jquery.dataTables.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/adminstyle.css">
+    <link rel="stylesheet" type="text/css" href="admin_asset/css/mycss.css">
+    <script src="admin_asset/ckeditor/ckeditor.js" type="text/javascript"></script>
+    <link href="source/assets/dest/css/bootstrap.min.css" rel="stylesheet">
+    <link href="source/assets/dest/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+    <link href="source/assets/dest/css/dashboard.css" rel="stylesheet">
+    <script src="source/assets/dest/js/ie8-responsive-file-warning.js"></script>
+    <script src="source/assets/dest/js/ie-emulation-modes-warning.js"></script>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+</head>
+
+<body>
+    <div id="wrapper">
+        @include('layout.header')
+        @yield('content')
+    </div>
+
+    <script src="admin_asset/bower_components/jquery/dist/jquery.min.js"></script>
+    <script src="admin_asset/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="admin_asset/bower_components/metisMenu/dist/metisMenu.min.js"></script>
+    <script src="admin_asset/dist/js/sb-admin-2.js"></script>
+    <script src="http://cdn.datatables.net/1.10.4/js/jquery.dataTables.min.js"></script>
+    <script src="admin_asset/ckeditor/ckeditor.js"></script>
+    <script src="admin_asset/js/myscript.js"></script>
+    <script type="text/javascript">
+       $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+    </script>
+
+    @yield('script')
+    <script>
+    $(document).ready(function() {
+        $('#dataTables-example').DataTable();
+    });
+    </script>
+</body>
+</html>
